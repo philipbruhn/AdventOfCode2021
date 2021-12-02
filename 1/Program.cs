fdb86be0e157563f799bf8058143ba3bf98dcffd
Philip Bruhn <philip.bruhn@gmail.com>
2021-12-02 09:55:51 +0100

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"input.txt");
+            int[] inputNum = Array.ConvertAll(input, s => int.Parse(s));
+
+            #region Part One
+            int numberOfIncreace = 0;
+            for (int i = 1; i < inputNum.Length; i++)
+            {
+                if (inputNum[i-1] < inputNum[i])
+                {
+                    numberOfIncreace++;
+                }
+            }
+            Console.WriteLine($"{numberOfIncreace}");
+            #endregion
+            #region Part Two
+            numberOfIncreace = 0;
+            int currentDepth, nextDepth;
+            for (int i = 0; i < inputNum.Length-3; i++)
+            {
+                currentDepth = inputNum[i] + inputNum[i + 1] + inputNum[i + 2];
+                nextDepth = inputNum[i + 1] + inputNum[i + 2] + inputNum[i + 3];
+                if (currentDepth < nextDepth)
+                {
+                    numberOfIncreace++;
+                }
+                
+            }
+            Console.WriteLine($"{numberOfIncreace}");
+            #endregion
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"input.txt");
+            #region Part One
+            int horizontal = 0;
+            int vertical = 0;
+            foreach(var dir in input)
+            {
+                string[] dirParts = dir.Split(" ");
+                switch (dirParts[0])
+                {
+                    case "forward":
+                        horizontal += int.Parse(dirParts[1]);
+                        break;
+                    case "back":
+                        horizontal -= int.Parse(dirParts[1]);
+                        break;
+                    case "up":
+                        vertical -= int.Parse(dirParts[1]);
+                        break;
+                    case "down":
+                        vertical += int.Parse(dirParts[1]);
+                        break;
+                }
+            }
+            Console.WriteLine($"{vertical * horizontal}");
+            #endregion
+            #region Part Two
+            horizontal = 0;
+            vertical = 0;
+            int aim = 0;
+
+            foreach (var dir in input)
+            {
+                string[] dirParts = dir.Split(" ");
+                switch (dirParts[0])
+                {
+                    case "forward":
+                        horizontal += int.Parse(dirParts[1]);
+                        vertical += aim * int.Parse(dirParts[1]);
+                        break;
+                    case "back":
+                        horizontal -= int.Parse(dirParts[1]);
+                        vertical -= aim * int.Parse(dirParts[1]);
+                        break;
+                    case "up":
+                        aim -= int.Parse(dirParts[1]);
+                        break;
+                    case "down":
+                        aim += int.Parse(dirParts[1]);
+                        break;
+                }
+            }
+            Console.WriteLine($"{vertical * horizontal}");
+            #endregion
+        }
+    }
+}
