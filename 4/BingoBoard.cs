bc0ae07d59a5b58bd3109f837a1ef857076d4c35
Philip Bruhn <philip.bruhn@gmail.com>
2021-12-04 18:34:24 +0100

    Day 4

---
+++ 4/BingoBoard.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class BingoBoard
+    {
+        public BingoBoard(string[] boardStrings)
+        {
+            ConvertToDictItems(boardStrings);
+        }
+
+        public Dictionary<(int x, int y), (int value, bool isChecked)> Board { get; set; } = new();
+        
+        private void ConvertToDictItems(string[] boardStrings)
+        {
+            for(int i = 0; i < boardStrings.Length; i++)
+            {
+                List<string> rowString = boardStrings[i]
+                    .Split(" ")
+                    .Where(x => !x.Equals(""))
+                    .ToList();
+
+                for(int j = 0; j < rowString.Count; j++)
+                {
+                    Board.Add((i, j),(int.Parse(rowString[j]), false));
+                }
+            }
+        }
+
+        public bool HasBoardBingo((int x, int y) position)
+        {
+            if(HasRowBingo(position.x) || HasColumnBingo(position.y))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool TryMarkValue(int value, out (int, int) markedItemKey)
+        {
+            markedItemKey = (-1,-1);
+            if (Board.ContainsValue((value,false)))
+            {
+                var markedItem = Board.FirstOrDefault(a => a.Value == (value, false));
+                markedItemKey = markedItem.Key;
+                Board[markedItemKey] = (value, true) ;
+                return true;
+            }
+            return false;
+        }
+
+        private bool HasRowBingo(int x)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (!Board.GetValueOrDefault((x, i)).isChecked)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool HasColumnBingo(int y)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (!Board.GetValueOrDefault((i, y)).isChecked)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"input.txt");
+            #region Format Input
+            int[] numbersToDraw = Array.ConvertAll(input[0].Split(","), s => int.Parse(s));
+            string[] boardStrings = new string[5];
+            int boardRow = 0;
+            List<BingoBoard> boards = CreateListWithBoards(input);
+            List<BingoBoard> CreateListWithBoards(string[] input)
+            {
+                List<BingoBoard> bingoBoards = new();
+                for (int i = 2; i < input.Length; i++)
+                {
+                    if (!input[i].Equals(""))
+                    {
+                        boardStrings[boardRow] = input[i];
+                        boardRow++;
+                    }
+                    if (boardRow > 4)
+                    {
+                        bingoBoards.Add(new BingoBoard(boardStrings));
+                        boardRow = 0;
+                    }
+                }
+                return bingoBoards;
+            }
+            #endregion
+            #region Part One
+            int PlayBingo()
+            {
+                foreach (var number in numbersToDraw)
+                {
+                    foreach (var board in boards)
+                    {
+                        if (board.TryMarkValue(number, out (int, int) position))
+                        {
+                            if (board.HasBoardBingo(position))
+                            {
+                                int sum = 0;
+                                foreach(var item in board.Board.Where(a => !a.Value.isChecked))
+                                {
+                                    sum += item.Value.value;
+                                }
+                                return sum * number;
+                            }
+                        }
+                    }
+                }
+                return 0;
+            }
+
+            Console.WriteLine($"{PlayBingo()}");
+            #endregion
+            #region PartTwo
+            boards = CreateListWithBoards(input);
+            Console.WriteLine($"{PlayBingoToLose()}");
+
+            int PlayBingoToLose()
+            {
+                foreach (var number in numbersToDraw)
+                {
+                    List<BingoBoard> boardsWithBingo = new();
+                    for (int i = 0; i < boards.Count; i++)
+                    {
+                        if (boards[i].TryMarkValue(number, out (int, int) position))
+                        {
+                            if (boards[i].HasBoardBingo(position))
+                            {
+                                if(boards.Count < 2)
+                                {
+                                    int sum = 0;
+                                    foreach (var item in boards[i].Board.Where(a => !a.Value.isChecked))
+                                    {
+                                        sum += item.Value.value;
+                                    }
+                                    return sum * number;
+                                }
+                                else
+                                {
+                                    boardsWithBingo.Add(boards[i]);
+                                }
+                            }
+                        }
+                    }
+                    foreach(var board in boardsWithBingo)
+                    {
+                        boards.Remove(board);
+                    }
+                }
+                return 0;
+            }
+            #endregion
+        }
+    }
+}
