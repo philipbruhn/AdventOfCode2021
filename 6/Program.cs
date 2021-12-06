97f8f439d19884cbe0a22bdfaf52c0dff6215338
Philip Bruhn <philip.bruhn@gmail.com>
2021-12-06 17:03:45 +0100

    Day 6

---
+++ 6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllText(@"input.txt").Split(",");
+            List<int> fishes = Array.ConvertAll(input, s => int.Parse(s)).ToList();
+
+            Console.WriteLine($"{GetFishesInAmountOfDays(80)}");
+            Console.WriteLine($"{GetFishesInAmountOfDays(256)}");
+
+            double GetFishesInAmountOfDays(int days)
+            {
+                double fishCount = fishes.Count;
+                double[] newFishesOnDay = new double[days];
+                foreach(var fish in fishes)
+                {
+                    newFishesOnDay[fish]++;
+                }
+                for (int i = 1; i < days; i++)
+                {
+                    fishCount += newFishesOnDay[i];
+                    if(newFishesOnDay.Length -1 > i + 6)
+                    {
+                        newFishesOnDay[i + 7] += newFishesOnDay[i];
+                    }
+                    if (newFishesOnDay.Length - 1 > i + 8)
+                    {
+                        newFishesOnDay[i + 9] += newFishesOnDay[i];
+                    }
+                }
+                return fishCount;
+            }
+        }
+    }
+}
